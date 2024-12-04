cef33a83abe367d039a6cb1f07a123f659dcd34b
Goran <goranbubalo2422003@gmail.com>
2024-12-04 16:48:59 +0100

    loop for decreasing the number from 10 to 2

--- RS1/zadatak5.py
+++ RS1/zadatak5.py
@@ -2,7 +2,7 @@
 
 #User input of the number 
 the_number = int(input("Input a number: "))
-the_result = 1
+the_result = 1 
 
 for i in range(1,the_number + 1):
     the_result *= i
---
+++ RS1/zadatak6.py
@@ -0,0 +1,14 @@
+for i in range(1, 2):
+  print(i)
+  # No need for a loop, we have a range of only 1 
+
+print()
+
+for i in range(1, 10, 2):
+  print(i)
+  #Output odd numbers 
+  
+print()
+for i in range(10, 1, -1):
+  print(i)
+  #loop -> numbers decrease from 10 to 2 
