ae1369ef2a053d19173ff767c2712b461b29a7d5
Goran <goranbubalo2422003@gmail.com>
2024-12-04 16:14:52 +0100

    Guess the output

--- RS1/zadatak4_1.py
+++ RS1/zadatak4_1.py
@@ -1,6 +1,10 @@
-#Guess the output 
-  broj = 0 #Setting the number 
+#Guess the output 2
+
+broj = 0 #Setting the number 
 while broj < 5: #Loop is running until the number is higher then 5
   broj += 1 #Increment the number by 1 
   print(broj) #Output
   broj -= 1 #Decrement 
+  
+  #The output is 111....
+  
